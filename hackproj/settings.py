7db5c387cc6d85dc1439528224cc924900b4baa2
SENAPATI SAI RAMYA <135352508+ramyramya@users.noreply.github.com>
2023-08-25 13:59:39 +0530

    Update settings.py

--- hackproj/settings.py
+++ hackproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u1ln^&f+$grakswhmo)!ci%#$$ch(=c!bi27==1$5*ur3mfhuf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['15.206.153.172', 'localhost', '0.0.0.0']
 
 
 # Application definition
